cf1c1ab25248bff600d9c7c02d8165238a0e0325
Deyvson Aguiar <deyvsonaguiar@gmail.com>
2022-03-27 14:13:01 -0400

    Base projeto - compilando com typescript

---
+++ index.js
@@ -0,0 +1,4 @@
+function enviarEmail(nome, email) {
+    console.log(`Enviar email para ${nome} com email ${email}`);
+}
+enviarEmail("Deyvson", "deyvsonaguiar@gmail.com");
---
+++ index.ts
@@ -0,0 +1,5 @@
+function enviarEmail(nome: String, email: string) {
+    console.log(`Enviar email para ${nome} com email ${email}`)
+}
+
+enviarEmail("Deyvson", "deyvsonaguiar@gmail.com");
